0237c34e022d24aa9b88b1af822633e6d981dde7
pnovoa <pavel.novoa@gmail.com>
2020-03-17 14:55:31 -0300

    shell improved again

--- run_experiment.sh
+++ run_experiment.sh
@@ -7,7 +7,7 @@ declare -a CHANGE_TYPE=(1 2 3)
 declare -a FUTURE_HORIZON=(2 6 10)
 declare -a ALGORITHM=(1)
 
-MAX_RUNS=20
+MAX_RUNS=5
 SEED=12
 MAX_CHANGES=23
 CHANGE_FREQUENCY=2500
@@ -20,7 +20,7 @@ RESULT_ID=0
 for i in "${CHANGE_TYPE[@]}" ; do
     for j in "${FUTURE_HORIZON[@]}" ; do
       for k in "${ALGORITHM[@]}" ; do
-        for RUN_ID in {1...${MAX_RUNS}} ; do
+        for RUN_ID in {1..${MAX_RUNS}} ; do
           ((RESULT_ID+=1))
           OUTPUT_FILE="${OUTPUT_FOLDER}RESULT_${RESULT_ID}.CSV"
           ARG_LIST+=("-jar ${JAR_FILE} ${RUN_ID} ${SEED} ${MAX_CHANGES} ${CHANGE_FREQUENCY} ${i} ${j} ${k} ${POPULATION_SIZE} ${OUTPUT_FILE}")
