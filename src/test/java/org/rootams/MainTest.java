b2e93619949ca9b8df68e8b90bc70037f6eeca6f
pnovoa <pavel.novoa@gmail.com>
2020-03-30 22:07:02 -0300

    problem random seed is fixed (again)

--- src/test/java/org/rootams/MainTest.java
+++ src/test/java/org/rootams/MainTest.java
@@ -7,32 +7,32 @@ public class MainTest {
     @Test
     public void main() {
 
-        int MAX_RUNS = 30;
-
-        Main.SEED = 12;
-        Main.MAX_CHANGES = 50;
-        Main.CHANGE_FREQUENCY = 2500;
-        Main.CHANGE_TYPE = 1;
-        Main.FUTURE_HORIZON = 2;
-        Main.ALGORITHM_ID = 4;
-        Main.POPULATION_SIZE = 50;
-        Main.OUTPUT_FILE = "test.out";
-
-        Main.printHeadOfOutputFile();
-
-        for (int i = 0; i < MAX_RUNS; i++) {
-
-            Main.RUN_ID = i;
-
-            Main.runGlobalAMForEachEnvironment();
-
-        }
+//        int MAX_RUNS = 30;
+//
+//        Main.SEED = 12;
+//        Main.MAX_CHANGES = 50;
+//        Main.CHANGE_FREQUENCY = 2500;
+//        Main.CHANGE_TYPE = 1;
+//        Main.FUTURE_HORIZON = 2;
+//        Main.ALGORITHM_ID = 4;
+//        Main.POPULATION_SIZE = 50;
+//        Main.OUTPUT_FILE = "test.out";
+//
+//        Main.printHeadOfOutputFile();
+//
+//        for (int i = 0; i < MAX_RUNS; i++) {
+//
+//            Main.RUN_ID = i;
+//
+//            Main.runGlobalAMForEachEnvironment();
+//
+//        }
 
     }
 
     @Test
     public void mainJin() {
-        Main.RUN_ID = 1;
+        /*Main.RUN_ID = 1;
         Main.SEED = 1;
         Main.MAX_CHANGES = 23;
         Main.CHANGE_FREQUENCY = 2500;
@@ -43,7 +43,7 @@ public class MainTest {
         Main.OUTPUT_FILE = "test_jin.out";
 
         // Main.main(null);
-
+*/
 
 
     }
